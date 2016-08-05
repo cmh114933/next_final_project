a49a8795796ceb5d3c2c12d11df3a35a7970ee36
n"natalia-kochanska <wczorajszy_zgon@wp.pl>
2016-08-05 10:31:22 +0800

    edit form added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update]
 
 	def create
 		@user = User.last.id
@@ -8,7 +9,22 @@ class UsersController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+  	if @user.update(user_params)
+  		redirect_to user_path
+  	else
+  		render 'edit'
+  	end  
+  end
+
   private
+
+  def set_listing
+    @listing = User.find(params[:id])
+  end 
 
 	def user_params
     params.require(:user).permit(:username, :password, :token, :IGC_total, :IGC_base)
